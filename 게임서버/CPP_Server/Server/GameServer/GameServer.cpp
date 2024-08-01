40d1aa62748b4bd958747e066c0dc11cf708f32f
MinSeok <hen0406@naver.com>
2024-08-01 13:54:24 +0900

    파일관리_v0

---
+++ 게임서버/CPP_Server/Server/GameServer/GameServer.cpp
@@ -0,0 +1,56 @@
+﻿#include "pch.h"
+#include <iostream>
+#include "CorePch.h"
+#include <thread>
+#include <atomic>		
+#include <mutex>
+#include <Windows.h>
+#include <future>
+#include "ThreadManager.h"
+
+#include "RefCounting.h"
+#include "Memory.h"
+#include "Allocator.h"
+
+class Knight
+{
+public:
+	Knight()
+	{
+		cout << "Knight()" << endl;
+	}
+
+	Knight(int32 hp) : _hp(hp)
+	{
+		cout << "Knight(hp)" << endl;
+	}
+
+	~Knight()
+	{
+		cout << "~Knight()" << endl;
+	}
+
+	int32 _hp = 100;
+	int32 _mp = 10;
+};
+
+int main()
+{
+	for (int32 i = 0; i < 5; i++)
+	{
+		GThreadManager->Launch([]()
+			{
+				while (true)
+				{
+					Vector<Knight> v[10];
+
+					Map<int32, Knight> m;
+					m[100] = Knight();
+
+					this_thread::sleep_for(100ms);
+				}
+			});
+	}
+
+	GThreadManager->Join();
+}
---
+++ 게임서버/CPP_Server/Server/ServerCore/CorePch.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "Types.h"
+#include "CoreGlobal.h"
+#include "CoreTLS.h"
+#include "CoreMacro.h"
+#include "Container.h"
+
+#include <Windows.h>
+#include <iostream>
+using namespace std;
+
+#include "Lock.h"
