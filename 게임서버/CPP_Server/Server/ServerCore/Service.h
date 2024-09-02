925199a0a097aa180d810816c61fff582c116fd7
MinSeok <hen0406@naver.com>
2024-09-03 00:07:32 +0900

    Server Service

---
+++ 게임서버/CPP_Server/Server/ServerCore/Service.h
@@ -0,0 +1,84 @@
+#pragma once
+#include "NetAddress.h"
+#include "IocpCore.h"
+#include "Listener.h"
+#include <functional>
+
+enum class ServiceType : uint8
+{
+	Server,
+	Client
+};
+
+/*----------------
+	Service
+----------------*/
+
+using SessionFactory = function<SessionRef(void)>;
+
+class Service : public enable_shared_from_this<Service>
+{
+public:
+	Service(ServiceType type, NetAddress address, IocpCoreRef core, SessionFactory factory, int32 maxSessionCount = 1);
+	virtual ~Service();
+
+	virtual bool			Start() abstract;
+	bool					CanStart() { return _sessionFactory != nullptr; }
+
+	virtual void			CloseService();
+	void					SetSessionFactory(SessionFactory func) { _sessionFactory = func; }
+
+	SessionRef				CreateSession();
+	void					AddSession(SessionRef session);
+	void					ReleaseSession(SessionRef session);
+	int32					GetCurrentSessionCount() { return _sessionCount; }
+	int32					GetMaxSessionCount() { return _maxSessionCount; }
+
+public:
+	ServiceType				GetServiceType() { return _type; }
+	NetAddress				GetNetAddress() { return _netAddress; }
+	IocpCoreRef&			GetIocpCore() { return _iocpCore; }
+
+protected:
+	USE_LOCK;
+	ServiceType				_type; // 서버 서비스인지 클라이언트 서비스인지
+	NetAddress				_netAddress = {};
+	IocpCoreRef				_iocpCore;
+
+	Set<SessionRef>			_sessions; // 해당 서비스를 지원할 세션들을 관리
+	int32					_sessionCount = 0;
+	int32					_maxSessionCount = 0;
+	SessionFactory			_sessionFactory;
+};
+
+
+/*----------------
+	ClientService
+----------------*/
+
+class ClientService : public Service
+{
+public:
+	ClientService(NetAddress targetAddress, IocpCoreRef core, SessionFactory factory, int32 maxSessionCount = 1);
+	virtual ~ClientService() {}
+
+	virtual bool Start() override;
+};
+
+
+
+/*----------------
+	ServerService
+----------------*/
+class ServerService : public Service
+{
+public:
+	ServerService(NetAddress targetAddress, IocpCoreRef core, SessionFactory factory, int32 maxSessionCount = 1);
+	virtual ~ServerService() {}
+
+	virtual bool Start() override;
+	virtual void CloseService() override;
+
+private:
+	ListenerRef _listener = nullptr;
+};
